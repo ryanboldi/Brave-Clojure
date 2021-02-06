dff86880c417716c2ecd3a39981b0604a5eeb330
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-06 16:31:01 +0400

    2 and 3 done

--- do-things/main.clj
+++ do-things/main.clj
@@ -3,3 +3,18 @@
 (defn -main
   [& args]
   (println "Hello world!"))
+
+;exercise 2
+(def add_100 #(+ % 100))
+
+;exercise 3
+(defn dec-maker
+  "creates a custom decrementor"
+  [decrement]
+  #(- % decrement))
+
+(def dec9 (dec-maker 9))
+
+(dec9 10)
+
+;exercise 4
