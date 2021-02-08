25148a5bf7c2a1596c60e8cf45a689e49a4cf002
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-08 12:34:35 +0400

    implemented filter with reduce

--- core-functions/main.clj
+++ core-functions/main.clj
@@ -5,7 +5,6 @@
   (println "Hello world!"))
 
 ;Implement map using reduce
-
 (defn my_map
   "implements the map function using reduce"
   [func sequence]
@@ -15,3 +14,14 @@
           (seq sequence))))
 
 (my_map inc [1 2 3])
+
+;implement filter using reduce
+(defn my_filter
+  "implements the filter function using reduce"
+  [predicate_func sequence]
+  (seq (reduce (fn [good left]
+                 (if (predicate_func left)
+                   (into good [left])
+                   good))
+               []
+               (seq sequence))))
