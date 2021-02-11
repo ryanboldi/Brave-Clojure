501b66f607afaee047e49c49b3df20f6252ba064
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-11 09:05:19 +0400

    Update main.clj

--- core-functions/main.clj
+++ core-functions/main.clj
@@ -33,9 +33,9 @@
   "implements some using reduce"
   [predicate_func sequence]
   (some identity (reduce (fn [good left]
-                   (into good [(predicate_func left)]))
-               []
-               (seq sequence))))
+                           (into good [(predicate_func left)]))
+                         []
+                         (seq sequence))))
 
 (my_some #(> % 3) [1 2 9 3 10 54]) ; => true
 (my_some #(and (> % 3) %) [1 2 9 3 10 54]) ; => 9
@@ -43,3 +43,41 @@
 
 (def suspect_file "suspects.csv")
 (slurp suspect_file)
+
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
+
+(defn parse
+  "Convert a CSV into rows of columns"
+  [string]
+  (map #(clojure.string/split % #",")
+       (clojure.string/split string #"\r\n")))
+
+(parse (slurp suspect_file))
+
+(defn mapify
+  "Return a seq of maps like {:name \"Edward Cullen\" :glitter-index 10}"
+  [rows]
+  (map (fn [unmapped-row]
+         (reduce (fn [row-map [vamp-key value]]
+                   (assoc row-map vamp-key (convert vamp-key value)))
+                 {}
+                 (map vector vamp-keys unmapped-row)))
+       rows))
+
+
+(first (mapify (parse (slurp suspect_file))))
+
+(defn glitter-filter
+  [minimum-glitter records]
+  (filter #(>= (:glitter-index %) minimum-glitter) records))
