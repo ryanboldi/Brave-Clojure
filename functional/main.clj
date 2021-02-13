c241c28e5099520fef15ad5d1354e9975cc76992
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-13 12:36:33 +0400

    it works!

--- functional/main.clj
+++ functional/main.clj
@@ -1,24 +1,13 @@
-(ns main)
-
-(defn -main
-  [& args]
-  (println "Hello world!"))
-
-(defn two-comp
-  [f g]
-  (fn [& args]
-    (f (apply g args))))
-
-
-((two-comp inc +) 1 2 3 4)
-
 ;create comp function for any number of functions
 
 (defn my-comp
-  [f & others]
-  (fn [& args]
-    (println args)
-    (f (apply (my-comp others) args))))
+  ([f]
+   (fn [& args]
+     (apply f args)))
+  ([f & others]
+   (fn [& args]
+     (println args f others)
+     (f (apply (apply my-comp others) args)))))
 
-((my-comp +) 1 2 3 4)
+((my-comp inc inc inc +) 1 2 3 4) ; => 13
 ((comp inc inc inc +) 1 2 3 4) ; => 13
