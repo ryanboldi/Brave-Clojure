3938dfcd666e04656c8fd2e6856183d3f93ec397
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-13 12:22:21 +0400

    Update main.clj

--- functional/main.clj
+++ functional/main.clj
@@ -3,3 +3,22 @@
 (defn -main
   [& args]
   (println "Hello world!"))
+
+(defn two-comp
+  [f g]
+  (fn [& args]
+    (f (apply g args))))
+
+
+((two-comp inc +) 1 2 3 4)
+
+;create comp function for any number of functions
+
+(defn my-comp
+  [f & others]
+  (fn [& args]
+    (println args)
+    (f (apply (my-comp others) args))))
+
+((my-comp +) 1 2 3 4)
+((comp inc inc inc +) 1 2 3 4) ; => 13
