4e4d32f9001f73f0113d44b411eac08273795687
Ryan Boldi <Ryan.boldi123@gmail.com>
2021-02-14 13:52:57 +0400

    implemented assoc-in

--- functional/main.clj
+++ functional/main.clj
@@ -1,5 +1,24 @@
-;create comp function for any number of functions
+(ns main)
 
+;EXERCISES
+; 1: attr function that does the same as (comp :intelligence :attributes)
+
+(def character
+  {:name "Smooches McCutes"
+   :attributes {:intelligence 10
+                :strength 4
+                :dexterity 5}})
+
+(def c-int (comp :intelligence :attributes))
+(def c-str (comp :strength :attributes))
+(def c-dex (comp :dexterity :attributes))
+
+(def attr #(comp % :attributes))
+
+((attr :intelligence) character) ; => 10
+
+;create comp function for any number of functions
+; 2
 (defn my-comp
   ([f]
    (fn [& args]
@@ -13,3 +32,12 @@
 ((comp inc inc inc +) 1 2 3 4) ; => 13
 
 
+; implement the assoc-in function
+(defn my-assoc-in
+  [m [k & ks] v]
+  (if (empty? ks)
+    (assoc m k v)
+    (assoc {} k (my-assoc-in m ks v))))
+
+(assoc-in {} [:teams :team1 :leader :intelligence] 10) ; => {:teams {:team1 {:leader {:intelligence 10}}}}
+(my-assoc-in {} [:teams :team1 :leader :intelligence] 10) ; => {:teams {:team1 {:leader {:intelligence 10}}}}
