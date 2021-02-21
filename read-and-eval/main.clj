0661b60bd3487256b5597c01e92ef1c376baee01
Ryan Boldi <ryan.boldi123@gmail.com>
2021-02-21 15:15:12 +0400

    Update main.clj

--- read-and-eval/main.clj
+++ read-and-eval/main.clj
@@ -30,4 +30,4 @@
                                                                  (nth correct (inc (inc index))))) (drop (+ index 3) correct))))
           (recur (inc index) correct))))))
 
-(infix (1 + 2 * 3 + 5))
+(infix (1 - 2 * 3 - 8 * 9))
